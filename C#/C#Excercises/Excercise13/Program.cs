18f206163328910128d453e31ceedfbb8faec5f7
cuongph0207 <cuongphktos2003@gmail.com>
2024-03-06 14:18:29 +0700

    update

--- C#/C#Excercises/Excercise13/Program.cs
+++ C#/C#Excercises/Excercise13/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Excercise13
+﻿using System.Reflection;
+
+namespace Excercise13
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HocSinh hocSinh = new HocSinh();
+            hocSinh.name = "Cuong";
+            hocSinh.age = 20;
+            hocSinh.toString();
+        }
+        public struct HocSinh
+        {
+            private int id {  get; set; }
+            public string name {  get; set; }   
+            public int age {  get; set; } 
+
+            public void toString()
+            {
+                Console.WriteLine("Ten : {0} \nTuoi : {1}",name,age);
+            }
+
         }
     }
 }
+
+
---
+++ C#/C#Excercises/Excercise14/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Excercise14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.bookName = "De Men Phieu Luu Ky";
+            book.bookAuthorName = "To Hoai";
+            book.toString();
+            Book book2 = new Book("Tat den","Ngo tat to");
+            book2.toString();
+        }
+        public struct Book
+        {
+            public string bookName { get; set; }
+            public string bookAuthorName { get; set; }
+
+            public Book()
+            {
+
+            }
+            public Book(string bookName, string bookAuthorName)
+            {
+                this.bookName = bookName;
+                this.bookAuthorName = bookAuthorName;
+            }
+            public void toString()
+            {
+                Console.WriteLine("Ten sach : {0} \nTen tac gia : {1}",bookName,bookAuthorName);
+            }
+        }
+    }
+}
